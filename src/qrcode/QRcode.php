8ad1c3eba9c089c8bbd17809a2fb213b116fb983
LittleMo <25362583@qq.com>
2021-08-06 16:15:36 +0800

    准备引入qrcode相关功能

---
+++ src/qrcode/QRcode.php
@@ -0,0 +1,46 @@
+<?php
+// +----------------------------------------------------------------------
+// | Little Mo - Tool [ WE CAN DO IT JUST TIDY UP IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2011 http://ggui.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: littlemo <25362583@qq.com>
+// +----------------------------------------------------------------------
+
+namespace littlemo\tool\qrcode;
+
+use chillerlan\QRCode\QRCode as BaseQRcode;
+use chillerlan\QRCode\QROptions;
+
+class QRcode
+{
+    public function imgPng()
+    {
+    }
+    public function imgJpg()
+    {
+    }
+    public function imgGif()
+    {
+    }
+    public function markSvg()
+    {
+    }
+    public function markHtml()
+    {
+    }
+    public function strJson()
+    {
+    }
+    public function strText()
+    {
+    }
+    public function imagick()
+    {
+    }
+    public function custom()
+    {
+    }
+}
