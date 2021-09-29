4475ba921ca23299f71838986fc0abc98a06967e
LittleMo <25362583@qq.com>
2021-09-29 10:59:58 +0800

    新增HttpClient

---
+++ src/HttpClient.php
@@ -0,0 +1,243 @@
+<?php
+// +----------------------------------------------------------------------
+// | Little Mo - Tool [ WE CAN DO IT JUST TIDY UP IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2021 http://ggui.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: littlemo <25362583@qq.com>
+// +----------------------------------------------------------------------
+
+namespace littlemo\tool;
+
+/**
+ * Http Client
+ */
+
+class HttpClient
+{
+
+    /**
+     * http 报文设置
+     */
+    static $headers = [];
+
+    /**
+     * cURL允许执行的最长秒数
+     */
+    static $connectTimeout = 60;
+
+    /**
+     * cURL允许响应的最长秒数
+     */
+    static $socketTimeout = 60;
+    /**
+     * headers
+     */
+    static $conf = [];
+
+    /**
+     * HttpClient
+     * @param array $headers HTTP header
+     */
+    public function __construct($headers = array(), $connectTimeout = 60000, $socketTimeout = 60000, $conf = [])
+    {
+        self::$headers = self::buildHeaders($headers);
+        self::$connectTimeout = $connectTimeout;
+        self::$socketTimeout = $socketTimeout;
+        self::$conf = $conf;
+    }
+
+    /**
+     * 连接超时
+     * @param int $ms 毫秒
+     */
+    public function setConnectionTimeoutInMillis($ms)
+    {
+        self::$connectTimeout = $ms;
+    }
+
+    /**
+     * 响应超时
+     * @param int $ms 毫秒
+     */
+    public function setSocketTimeoutInMillis($ms)
+    {
+        self::$socketTimeout = $ms;
+    }
+
+    /**
+     * 配置
+     * @param array $conf
+     */
+    public function setConf($conf)
+    {
+        self::$conf = $conf;
+    }
+
+    /**
+     * 请求预处理
+     * @param resource $ch
+     */
+    public function prepare($ch)
+    {
+        foreach (self::$conf as $key => $value) {
+            curl_setopt($ch, $key, $value);
+        }
+    }
+
+    /**
+     * @param  string $url
+     * @param  array $data HTTP POST BODY
+     * @param  array $param HTTP URL
+     * @param  array $headers HTTP header
+     * @return array
+     */
+    public function post($url, $data = array(), $params = array(), $headers = array())
+    {
+        $url = $this->buildUrl($url, $params);
+        $headers = array_merge(self::$headers, self::buildHeaders($headers));
+
+        $ch = curl_init();
+        $this->prepare($ch);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, is_array($data) ? http_build_query($data) : $data);
+        curl_setopt($ch, CURLOPT_TIMEOUT, self::$socketTimeout);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, self::$connectTimeout);
+        $content = curl_exec($ch);
+        $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+        if ($code === 0) {
+            $error_des = curl_error($ch);
+        }
+
+        curl_close($ch);
+        return array(
+            'code' => $code,
+            'content' => $content,
+            'error_des' => $error_des ?? '',
+        );
+    }
+
+    /**
+     * @param  string $url
+     * @param  array $datas HTTP POST BODY
+     * @param  array $param HTTP URL
+     * @param  array $headers HTTP header
+     * @return array
+     */
+    public function multi_post($url, $datas = array(), $params = array(), $headers = array())
+    {
+        $url = $this->buildUrl($url, $params);
+        $headers = array_merge(self::$headers, self::buildHeaders($headers));
+
+        $chs = array();
+        $result = array();
+        $mh = curl_multi_init();
+        foreach ($datas as $data) {
+            $ch = curl_init();
+            $chs[] = $ch;
+            $this->prepare($ch);
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_HEADER, false);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, is_array($data) ? http_build_query($data) : $data);
+            curl_setopt($ch, CURLOPT_TIMEOUT, self::$socketTimeout);
+            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, self::$connectTimeout);
+            curl_multi_add_handle($mh, $ch);
+        }
+
+        $running = null;
+        do {
+            curl_multi_exec($mh, $running);
+            usleep(100);
+        } while ($running);
+
+        foreach ($chs as $ch) {
+            $content = curl_multi_getcontent($ch);
+            $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            $result[] = array(
+                'code' => $code,
+                'content' => $content,
+            );
+            curl_multi_remove_handle($mh, $ch);
+        }
+        curl_multi_close($mh);
+
+        return $result;
+    }
+
+    /**
+     * @param  string $url
+     * @param  array $param HTTP URL
+     * @param  array $headers HTTP header
+     * @return array
+     */
+    public function get($url, $params = array(), $headers = array())
+    {
+        $url = $this->buildUrl($url, $params);
+        $headers = array_merge(self::$headers, self::buildHeaders($headers));
+
+        $ch = curl_init();
+        $this->prepare($ch);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_TIMEOUT, self::$socketTimeout);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, self::$connectTimeout);
+        $content = curl_exec($ch);
+        $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+        if ($code === 0) {
+            $error_des = curl_error($ch);
+        }
+
+        curl_close($ch);
+        return array(
+            'code' => $code,
+            'content' => $content,
+            'error_des' => $error_des ?? '',
+        );
+    }
+
+    /**
+     * 构造 header
+     * @param  array $headers
+     * @return array
+     */
+    private static function buildHeaders($headers)
+    {
+        $result = array();
+        foreach ($headers as $k => $v) {
+            $result[] = sprintf('%s:%s', $k, $v);
+        }
+        return $result;
+    }
+
+    /**
+     * 
+     * @param  string $url
+     * @param  array $params 参数
+     * @return string
+     */
+    private function buildUrl($url, $params)
+    {
+        if (!empty($params)) {
+            $str = http_build_query($params);
+            return $url . (strpos($url, '?') === false ? '?' : '&') . $str;
+        } else {
+            return $url;
+        }
+    }
+}
